1372808582a50403823ef2539d9b10de13622b22
mkpenta <mpenta@necc.mass.edu>
2023-12-04 06:17:26 -0500

    first part complete

--- 2023/day4/main.py
+++ 2023/day4/main.py
@@ -11,8 +11,23 @@ def get_data():
     with open("2023/day4/input", "r") as file:
         file_list = file.readlines()
         m = list(map(make_tuple, file_list))
+        data_list.extend(m)
     return data_list
 
 
+def make_intersections(data :list):
+    sets = []
+    for s1, s2 in data:
+        s = set(s1).intersection(set(s2))
+        sets.append(list(s))
+    return sets
+
+
 if __name__ == '__main__':
-    get_data()
+    d = get_data()
+    winning_numbers = make_intersections(d)
+    total = 0
+    for game in winning_numbers:
+        score = 2**(len(game)-1)
+        total += int(score)
+    print(total)
