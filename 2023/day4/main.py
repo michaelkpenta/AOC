9fd0ed099645f32752bfc3be57bf39345f269a35
mkpenta <mpenta@necc.mass.edu>
2023-12-04 05:47:13 -0500

    parse input into a list of tuples of lists

--- 2023/day4/main.py
+++ 2023/day4/main.py
@@ -1,3 +1,18 @@
+def make_tuple(x):
+    x = x.strip().split(":")
+    left, right = x[1].split("|")
+    left = left.strip().split()
+    right = right.strip().split()
+    return left, right
+
+
+def get_data():
+    data_list = []
+    with open("2023/day4/input", "r") as file:
+        file_list = file.readlines()
+        m = list(map(make_tuple, file_list))
+    return data_list
 
 
 if __name__ == '__main__':
+    get_data()
