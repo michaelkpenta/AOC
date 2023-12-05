7a229c91bbab182369c5d73559dc8960fd6eee60
mkpenta <mpenta@necc.mass.edu>
2023-12-05 11:00:44 -0500

    general convertor seems to be ok

--- 2023/day5/main.py
+++ 2023/day5/main.py
@@ -1,13 +1,13 @@
 
-
-def get_data():
-    data = {}
-    maps = ["seed-to-soil map:", "soil-to-fertilizer map:",
+maps = ["seed-to-soil map:", "soil-to-fertilizer map:",
             "fertilizer-to-water map:", "water-to-light map:",
             "light-to-temperature map:",
             "temperature-to-humidity map:", "humidity-to-location map:"]
 
-    data["seeds"] = []
+
+def get_data():
+    global maps
+    data = {"seeds": []}
     for i in range(len(maps)):
         data[maps[i]] = []
     current_map = maps[0]
@@ -26,7 +26,17 @@ def get_data():
 
         return data
 
+def check_in_range(translator_in, translator_name, data_dict):
+    translator: list = data_dict[translator_name]
+    for (des_start, src_start, distance) in translator:
+        if src_start <= translator_in < src_start + distance:
+            dist_to_src_start = translator_in - src_start
+            dest_result = des_start + dist_to_src_start
+            return dest_result
+    return translator_in #not found - returns its own value
 
 if __name__ == '__main__':
-    print(get_data())
+    d = get_data()
+    print(check_in_range(1, 'seed-to-soil map:', d))
+
 
